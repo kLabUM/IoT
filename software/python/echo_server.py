6c45ecb38ed1c6098e3f73b9f441edb8e964aa58
Yanglin Zhao <yazh@umich.edu>
2013-06-28 13:42:52 -0400

    Initial commit.

---
+++ software/python/echo_server.py
@@ -0,0 +1,17 @@
+import socket
+
+host = ''
+port = 54321
+backlog = 5
+size = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen(backlog)
+
+while 1:
+    client, address = s.accept()
+    data = client.recv(size)
+    if data:
+        client.send(data)
+    client.close()
---
+++ software/python/modem_mac.py
@@ -0,0 +1,74 @@
+import serial
+import time
+
+# Modem config
+modem_port = serial.Serial()
+modem_port.baudrate = 115200
+modem_port.port = "/dev/tty.usbserial-00002114B"
+modem_port.parity = "N"
+modem_port.stopbits = 1
+modem_port.xonxoff = 0
+modem_port.open()
+
+def send_at_command(command):
+    modem_port.write(bytes(command+"\r", encoding='ascii'))
+
+if modem_port.isOpen():
+
+    # Ready module
+    send_at_command("AT")
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    # Check network status
+    send_at_command("AT+CREG=1")
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    send_at_command("AT+CREG?")
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    # Check signal strength
+    send_at_command("AT+CSQ")
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    # Configure socket
+    send_at_command("AT#SCFG=1,1,0,0,600,0")
+    time.sleep(5)
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    # Obtain IP from network
+    send_at_command("AT#SGACT=1,1")
+    time.sleep(5)
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    # Connect
+    send_at_command("#AT#SD=1,0,10510,\"88.37.127.164\"")
+    time.sleep(5)
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print(modem_port.readline().decode('ascii').strip())
+    print("\n")
+
+    #AT#SD=1,0,10510,"88.37.127.164"
+    #AT#SD=1,0,35421,"50.19.242.87”
+    #AT#SD=1,0,54321,"68.40.205.20”
+
+modem_port.close()
