03be9ad7024cf6552a5caf6051269b95a160dc84
bpwong <bpwong@berkeley.edu>
2015-10-22 00:20:37 -0400

    Files: Remove Python and Ruby scripts for testing echo server, client on EC2

--- aws/ruby/client.rb
+++
@@ -1,9 +0,0 @@
-require 'socket'
-
-s = TCPSocket.new 'localhost', 2000
-
-while line = s.gets # Read lines from socket
-  puts line         # and print them
-end
-
-s.close             # close socket when done
