705a285b4d40e3eac5f000c177cda5a36c417e61
bpwong <bpwong@berkeley.edu>
2015-12-05 18:11:39 -0500

    task: Add more comments to main.c to improve readability/understanding

--- software/Sensor Node/psoc_datalogger_sms.cydsn/main.c
+++ software/Sensor Node/psoc_datalogger_sms.cydsn/main.c
@@ -16,9 +16,9 @@
 
 // define global variables
 #define WRITE_DEBUG 1
-#define FEED_ID 966630999
+#define FEED_ID 123456789
 
-const char *API_KEY = "7inkCfgjLCcSq6t2E3RC7OnmZykBUXEU8Yv7K56c7JYUOxdn";
+const char *API_KEY = "YOUR_KEY_HERE";
 char   data_packet[MAX_PACKET_LENGTH] = {0}, debug_packet[MAX_PACKET_LENGTH] = {0};
 uint8   packet_ready, take_reading;
 float   vBattery;
@@ -34,9 +34,6 @@ SolinstReading solinst_reading = {.valid = 0};          // Initialize SolinstRea
 void rtcCallBackReceived(uint8 alarmType);
 
 // prototype functions
-//void read_packet(uint8* packet, uint8 size);
-//void write_packet(uint8* dest, uint8* src_ultrasonic, uint8* src_pressure, uint8 size);
-//void clear_packet(char* packet);
 void Write_To_SD_Card(const char* fileName, const char* pMode, const void* pData, U32 NumBytes);
 CY_ISR_PROTO(isr_timer);
 void Goto_Low_Power_Mode();
@@ -46,15 +43,17 @@ void debugLog(const void *pData);
 void main(){
 
     CyGlobalIntEnable;
+
+    // Start up initial mote processes
     SleepTimer_Start();
     isr_SleepTimer_StartEx(sleepTimerWake_INT);
-    FS_Init();
+    FS_Init();  // SD Card
     modem_set_api_feed(FEED_ID, API_KEY);
     modem_start();
-    //modem_power_off();
     ultrasonic_start();
     solinst_start();
     ADC_SAR_1_Start();
+    //modem_power_off();
     
     packet_ready = 0u; 
     take_reading = 1u;
@@ -80,19 +79,36 @@ void main(){
     RTC_WriteTime(tm); //sets time
     */
 
+
     for(;;){
     
         RTC_Process_Tasks();
-         
+        
+        // Loop continuously and take a reading  
+        // every "t_sample" minutes.
+        // The variable "take_reading" is set to TRUE 
+        // by rtcCallBackReceived() defined after the for() loop
         if(take_reading){ 
 
+            // Turn on the modem to update state variables:
+            //  sampling frequency; triggering the autosampler
             modem_power_on();   
            
             if (clear_packet(data_packet)) {
                 packet_ready = 0u;
             }
                         
-            // modem_get_packet(data_packet);
+            // To communicate with the IoT platform, the node 
+            // 1) listens on a given port and 2) parses an incoming string 
+            // for relevant commands. Once a packet is ready to send (3), 
+            // the node 4) transmits the packet.  This is achieved in as little 
+            // as four lines of code by leveraging an existing TCP/IP library.
+            //
+            // 1) modem_get_packet()
+            // 2) packet_get_uint8()
+            // 3) sprintf(data_packet, ...
+            // 4) modem_send_packet()
+            //
             if (modem_get_packet(data_packet,"t_sample,trigger_sampler")) {
                 
                 // Read in any updated values
@@ -104,12 +120,14 @@ void main(){
                 }
             }
 
-
             /*
             if (send_attempts > 0) {
                 debug_write("New reading taken before previous packet was sent");
             }
             */
+
+            // Trigger the autosampler to collect the sample 
+            // if a sample is requested
             if (trigger_sampler){
                 
                 // Send in acknowledgement that packet containing
@@ -118,22 +136,24 @@ void main(){
                 
                 trigger_sampler = 0; // Update the value locally
                 
+                // Trigger the autosampler as long as there are still available samples
+                // If bottle_count >= MAX_BOTTLE_COUNT, skip taking a sample and avoid
+                // waiting for the code to timeout
                 if (bottle_count < MAX_BOTTLE_COUNT) {
                     // Turn off modem to conserve energy
                     modem_power_off();
                     
+                    // Start up the autosampler processes 
+                    // and power on the autosampler
                     autosampler_start();
                     autosampler_power_on();
                     
-                    /*
-                    if (autosampler_take_sample(&bottle_count) ) {
-                        sprintf(data_packet,"%s, %u\r\n",
-                            "bottle", bottle_count);
-                    }
-                    */ 
-                    
+
+                    // Trigger the autosampler and update current bottle_count
                     autosampler_take_sample(&bottle_count);
                     
+                    // Power off the autosampler
+                    // and shut down the autosampler processes 
                     autosampler_power_off(); 
                     autosampler_stop();    
                 }
@@ -142,12 +162,16 @@ void main(){
                 }
             }
             
-            // Get Readings for:
-            // depth, pressure, temp
-            // conductivity.
+            // Construct the data packet that will be transmitted
+            // by the sensor node, starting with the bottle the
+            // autosampler last collected a sample in
+            // (the packet is in .csv format)
             sprintf(data_packet,"%s, %u\r\n",
                 "bottle", bottle_count);
-                
+
+            // Get Sensor Readings for:
+            //  Depth, Pressure, Temperature, Conductivity
+            // and update the data packet                
             if (solinst_get_reading(&solinst_reading)){
                 sprintf(data_packet,"%s%s, %f\r\n", data_packet,
                     "depth_press", solinst_reading.depth);            
@@ -164,23 +188,10 @@ void main(){
                 sprintf(data_packet,"%s%s, %f\r\n", data_packet,
                     "V_batt", vBattery);              
             }
-            
-            /* Moved to after packet is written to server
-            NeoRtcTimeStruct tm_neo = NeoRTC_Read_Time();
 
-            sprintf(data_packet, "\r\n%d:%d:%d %d/%d/%d -- [ID %d] u_d=%f s_d=%f s_t=%f v_b=%f", tm_neo.hour, tm_neo.minute,
-                             tm_neo.second, tm_neo.day, tm_neo.month, tm_neo.year, moteID,
-                             ultrasonic_reading.depth, solinst_reading.depth, solinst_reading.temp,vBattery);
-            
-            Write_To_SD_Card("data.txt","a",data_packet,strlen(data_packet));
-            */
-            /* Old AWS packet
-            sprintf(data_packet, "\n%d:%d:%d %d/%d/%d -- [ID %d] u_d=%f s_d=%f s_t=%f v_b=%f", tm_neo.hour, tm_neo.minute,
-                             tm_neo.second, tm_neo.day, tm_neo.month, tm_neo.year, moteID,
-                             ultrasonic_reading.depth, solinst_reading.depth, solinst_reading.temp,vBattery);
-            */
-   
-            /* Create Xively Packet */
+            // An example of writing data in JSON format
+            // This was replaced with .csv format due to 
+            // .csv's smaller packet size
             /*
             sprintf(data_packet, "{"
                       "\"method\":\"put\","
@@ -196,19 +207,14 @@ void main(){
                       FEED_ID,API_KEY,
                       ultrasonic_reading.depth, solinst_reading.depth, solinst_reading.temp,0u,vBattery);
             */
-            /*
-            sprintf(data_packet, "%s, %f\r\n%s, %f\r\n%s, %f\r\n%s, %f",
-                "depth_sonic", ultrasonic_reading.depth,
-                "depth_press", solinst_reading.depth,
-                "temp_press", solinst_reading.temp,
-                "V_batt", vBattery
-                );
-            */
+
+            // Update flags for sending a packet and taking sensor readings
             packet_ready = 1u;
-            //send_attempts = 0u;
             take_reading = 0u;
-            
-  
+            //send_attempts = 0u;
+        
+        // Once the flag for packet_ready has been set, 
+        // send the packet and save the packet locally
         } else if(packet_ready){
             isr_SleepTimer_Stop;  //  <============= ADDRESS THIS WITH B.K.
             
@@ -216,76 +222,42 @@ void main(){
             modem_power_on();
             
             // modem_state should now be IDLE or READY
+            // and we can send the packet
             modem_send_packet(data_packet);
             
+            // Power off the modem and conserve energy
             modem_power_off();
             
             // Backup data to SD Card
+            // Use the current time to time-stamp the current reading
             NeoRtcTimeStruct tm_neo = NeoRTC_Read_Time();
 
+            // Overwrite current data_packet with time-stamped data
             sprintf(data_packet, "\r\n%d:%d:%d %d/%d/%d -- [ID %d] u_d=%f s_d=%f s_t=%f bottle=%d v_b=%f", tm_neo.hour, tm_neo.minute,
                              tm_neo.second, tm_neo.day, tm_neo.month, tm_neo.year, moteID,
                              ultrasonic_reading.depth, solinst_reading.depth, solinst_reading.temp,bottle_count,vBattery);
             
-            Write_To_SD_Card("data.txt","a",data_packet,strlen(data_packet));
-            
+            // Write data_packet to a *.txt file on the SD Card
+            Write_To_SD_Card("data.txt","a",data_packet,strlen(data_packet));            
+
+            // Clear the current packet in preparation for the next reading
+            // when the node awakens from sleep
             if (clear_packet(data_packet)) {
                 packet_ready = 0u;
             }
-            
-            /*
-            // else if(modem_state == MODEM_STATE_IDLE){
-            else { // modem state is ON (aka IDLE or READY)
-                
-                
-                ////if(modem_send_packet(data_packet)){     // if packet sent
-                //if(modem_send_packet(data_packet)){     // if packet sent
-                //    modem_power_off();
-                //    clear_packet(data_packet);
-                //}
-                //else{                                   // if packet not sent
-                //    modem_reset();
-                //    modem_power_off();
-                //}
-                
-                if(send_attempts < MAX_SEND_ATTEMPTS){
-                    
-                    send_attempts++;
-                    
-                    if (modem_send_packet(data_packet)){     
-                    // the packet is sent within the number of maximum attempts allowed
-                        //debug_write("Packet sent within max attempts.");
-                        modem_power_off();
-                        //debug_write("Modem powered off.");
-                        clear_packet(data_packet);
-                        send_attempts = 0u;
-                    }
-                    else {
-                        //modem_reset();
-                    }
-                    
-                } 
-                else {
-                // SEND ATTEMPTS MOVED TO modem.c
-                // the number of maximum attempts allowed has been exceeded
-                    modem_power_off();
-                    clear_packet(data_packet);
-                    send_attempts = 0u;
-                    debug_write("MAX_SEND_ATTEMPTS Exceeded");     
-                
-                }                
-                 
-            }
-            */
+        
+        // If either the sensor node should take a reading nor send a packet
+        // go to low power mode to increase battery life    
         }else{
-            NeoRTC_Set_Repeating_Minute_Alarm(t_sample);  // Set 1-minute alarm
+            // Set repeating alarm to trigger every "t_sample" minutes
+            NeoRTC_Set_Repeating_Minute_Alarm(t_sample);  
             Goto_Low_Power_Mode();
         }
     CyDelay(1u);  // Quick Pause to prevent locking
     }
 }
 
-//pulls down all pins relatd to mote to save power since we're nousing it
+// Pull down all pins related to mote to save power since we're not using it
 void killMotePower(){
     NEOMOTE_1_RX_RTS_n_Write(0u);
     NEOMOTE_1_RX_CTS_n_Write(0u);
@@ -303,21 +275,10 @@ CY_ISR(isr_timer){
     //timer_ReadStatusRegister();
 }
 
-//void read_packet(){
-//}
 
-/*
-void clear_packet(char* packet){
-    
-    //uint8 i;
-    //for(i = 0u; i < packet_size; i++){
-    //    packet[i] = 0u;
-    //}
-    
-    memset(packet,0,sizeof(packet));
-    packet_ready = 0u;
-}
-*/
+// This function is triggered by the RTC on the mote
+// Here, take_reading is set to TRUE once every t_sample minutes,
+// as set with NeoRTC_Set_Repeating_Minute_Alarm(t_sample);  
 void rtcCallBackReceived(uint8 alarmType){ // RTC Event Handler.
       
     if(alarmType == RTC_MINUTE_ALARM){
@@ -352,10 +313,4 @@ void Write_To_SD_Card(const char* fileName, const char* pMode, const void* pData
 
 }
 
-/*
-void debugLog(const void *pData){
-    if(WRITE_DEBUG)
-         Write_To_SD_Card("debugLog.txt", "a",  pData, strlen(pData));
-}
-*/
 /* [] END OF FILE */
